4e2c603cb8b6106649e7945cc2ca2a4ce8e4c6dc
emiliano0807 <emilianorivfacio@gmail.com>
2024-06-03 19:44:27 -0600

    Commit 3

---
+++ Prueba3.py
@@ -0,0 +1,12 @@
+# Description: This program creates a colorful spiral using the turtle module.
+import turtle
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+turtle.speed(0)
+turtle.bgcolor('black')
+
+for x in range(360):
+    turtle.pencolor(colors[x % len(colors)])
+    turtle.width(x / 100 + 1)
+    turtle.forward(x)
+    turtle.left(59)
